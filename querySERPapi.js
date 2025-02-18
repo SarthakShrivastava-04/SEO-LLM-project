6a7c23b54d6d2c5bfe8c7d51060f499e88f58f07
Sarthak Shrivastava <shrivastavasarthak18@gmail.com>
2025-02-18 18:34:49 +0530

    add gitignore

--- querySERPapi.js
+++ querySERPapi.js
@@ -6,15 +6,26 @@ dotenv.config();
 const querySERPapi = async (domain, country) => {
     const url = `https://api.dataforseo.com/v3/serp/google/organic?domain=${domain}&country=${country}`;    
 
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${process.env.DATAFORSEO_API_KEY}`,
-      },
-    });
-  
-    const data = await response.json();
-    return data;
+    const login = process.env.SERP_API_LOGIN;
+    const password = process.env.SERP_API_PASSWORD;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + btoa(`${login}:${password}`)
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 export default querySERPapi;
